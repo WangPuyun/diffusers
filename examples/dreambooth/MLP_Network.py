df2fd506b2e6e9f0b454334e00d4b2bb2722b983
WangPuyun <1007388259@qq.com>
2026-07-30 10:11:23 +0800

    Add initial standalone MLP training script

---
+++ examples/dreambooth/MLP_Network.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, dim, mlp_ratio=4.0, drop=0.0):
+        super().__init__()
+        hidden_dim = int(dim * mlp_ratio)
+        self.fc1 = nn.Linear(dim, hidden_dim)
+        self.act = nn.GELU()
+        self.drop1 = nn.Dropout(drop)
+        self.fc2 = nn.Linear(hidden_dim, dim)
+        self.drop2 = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
